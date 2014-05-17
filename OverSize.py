a46260b51ffb902adc50a6cc996eac810b628129
Jeremy Stilwell <jerstil93@gmail.com>
2014-05-17 16:11:10 -0400

    stores file now need to get username for emailing

--- OverSize.py
+++ OverSize.py
@@ -1,15 +1,33 @@
 __author__ = 'Jeremy Stilwell'
+import commands
 import os
 
 warningSize = "75000c"
 deleteSize = "100000c"
 
+#os.system("find '/home/jstilwell/test' -size +" + warningSize)
 print "files 50000 and above"
-os.system("find '/home/jstilwell/test' -size +" + warningSize)
-os.popen("find '/home/jstilwell/test' -size +" + warningSize)
+f = commands.getoutput("find '/home/jstilwell/test' -size +" + warningSize)
+
+#name of file being built
+varfilename = ""
+
+#holds all filenames
+fileArray = []
+for i in f:
+    if i == '\n':
+        fileArray.append(varfilename)
+        varfilename = ""
+    else:
+        varfilename += i
+    print varfilename
 
 
+print "f: ", f
+print "array: ", fileArray
 
+
+print
 print "files 100000 and above"
 os.system("find '/home/jstilwell/test' -size +" + deleteSize)
 #returns exit status
