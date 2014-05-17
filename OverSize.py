641a442b54b6d70276c2a2e8415e002bfe2e9686
Jeremy Stilwell <jerstil93@gmail.com>
2014-05-17 16:48:48 -0400

    stores file now need to get username for emailing

--- OverSize.py
+++ OverSize.py
@@ -7,32 +7,31 @@ deleteSize = "100000c"
 
 #os.system("find '/home/jstilwell/test' -size +" + warningSize)
 print "files 50000 and above"
-f = commands.getoutput("find '/home/jstilwell/test' -size +" + warningSize)
-
-#name of file being built
-varfilename = ""
+warn = commands.getoutput("find '/home/jstilwell/test' -size +" + warningSize)
+delete = commands.getoutput("find '/home/jstilwell/test' -size +" + deleteSize)
 
 #holds all filenames
 fileArray = []
+
+#turn the output of find into an array
 
 
 def turn_array(com):
     #iterator
     j = 0
 
+    #stores the name of file being built
     global varfilename
+    varfilename = ""
 
     for i in com:
         varfilename += i
-        if i == '\n' or j == (f.__len__() - 1):
+        if i == '\n' or j == (com.__len__() - 1):
             varfilename = varfilename.rstrip('\n')
             fileArray.append(varfilename)
             varfilename = ""
-        #print "vfn: ", varfilename
         j += 1
-        #print "j: ", j
-
-#print "f: ", f
+turn_array(warn)
 print "array: ", fileArray
 
 print
