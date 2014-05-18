425f90daaaaea952bc0bc6ac7b36825da3469ebc
Jeremy Stilwell <jerstil93@gmail.com>
2014-05-18 17:17:15 -0400

    should be able to email users about files but can't really test yet

--- OverSize.py
+++ OverSize.py
@@ -1,17 +1,20 @@
 __author__ = 'Jeremy Stilwell'
 import commands
-
+from sys import stdin
 
 warningSize = "50000c"
 deleteSize = "100000c"
 
-warn = commands.getoutput("find '/home/jstilwell/test' -size +" + warningSize)
-delete = commands.getoutput("find '/home/jstilwell/test' -size +" + deleteSize)
+
+
 
 #holds all filenames
 fileArray = []
-user_array = []
+userArray = []
 #turns the output of find into an array
+"""
+turns the data from a find command in to an array
+"""
 
 
 def turn_array(com):
@@ -31,26 +34,78 @@ def turn_array(com):
         j += 1
 
 
+"""
+finds the users from an array of files passed in
+"""
+
+
 def find_users(files):
     for i in files:
         len1 = i.__len__()
         temp1 = i[6:len1]
         temp2 = (temp1.partition('/'))
-        user_array.append(temp2[0])
+        userArray.append(temp2[0])
 
-print "files 50000 and above"
-turn_array(warn)
-find_users(fileArray)
-print fileArray
-print user_array
-print
-fileArray = []
-user_array = []
 
+def size_error():
+    print "Size of file list and user list are not the same "
+    exit()
+
+#echo cat | mutt -s "this is a test" jstilwell@agora.cs.wcu.edu
+########################################################################################################################
+delete = commands.getoutput("find '/home/jstilwell/test' -size +" + deleteSize)
+########################################################################################################################
 print "files 100000 and above"
 turn_array(delete)
 find_users(fileArray)
 print fileArray
-print user_array
+print userArray
+########################################################################################################################
+if len(fileArray) == len(userArray):
+    print
+    for j in range(0, len(fileArray)):
+        print "User: ", userArray[j], " File: ", fileArray[j], "will be deleted."
+    print "Delete Files [y/N]: "
+    stdin.read(1)
+    userinput = stdin.readline()
+
+    for j in range(0, len(fileArray)):
+        #f = commands.getstatusoutput("mv " + fileArray[j] + " ./trash")
+        #message for the user
+        msg = "Your file " + fileArray[j] + " has been removed because it was larger than the allowed size.\n " \
+                                            "Please contact your system administrator if you have questions. " \
+                                            "This is an automated message."
+        commands.getstatusoutput("echo -e " + msg + " | mutt -s 'Your File was Removed' " + userArray[j] +
+                                 "@agora.cs.wcu.edu")
+        f = "mv " + fileArray[j] + " ./trash"
+        print f
+    print
+    fileArray = []
+    userArray = []
+else:
+    size_error()
+########################################################################################################################
+warn = commands.getoutput("find '/home/jstilwell/test' -size +" + warningSize)
+########################################################################################################################
+print "files 50000 and above"
+turn_array(warn)
+find_users(fileArray)
+print fileArray
+print userArray
+########################################################################################################################
+if len(fileArray) == len(userArray):
+    print
+    for j in range(0, len(fileArray)):
+        msg = "Your file " + fileArray[j] + "is larger than the recommended size.\n " \
+                                            "Please contact your system administrator if you have questions. " \
+                                            "This is an automated message."
+        commands.getstatusoutput("echo -e " + msg + " | mutt -s 'Your File is Oversize' " + userArray[j] +
+                                 "@agora.cs.wcu.edu")
+else:
+    size_error()
+########################################################################################################################
+print
+
+
 
 #TODO email and delete files maybe not email user twice by doing bigger files first and deleteing then run the smaller find
